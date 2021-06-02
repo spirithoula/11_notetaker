01f16b922583d210d51ab15e6fd7ed4a73cf1e86
Alex Harrington <spirithoula2@gmail.com>
2021-06-01 20:52:39 -0500

    changes to server file

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,17 @@
+
 const express = require("express");
-const htmlRoutes = require("./routes/htmlRoute");
-const apiRoutes = require("./routes/apiRoute");
+const fs = require("fs");
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
 
-app.listen(PORT,()=> console.log(`Listening on PORT : ${PORT}`)); 
+require('./routes/routes')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
